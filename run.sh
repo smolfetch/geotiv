d6c4c601b3f5120d394d4e0af2512e2c6a473ef3
Trim Bresilla <trim.bresilla@gmail.com>
2025-05-20 04:48:06 +0200

    build: initialize project with CMake build system
    - Ignore `compile_commands.json`
    - Add CMake build system with component and project setup, dependency fetching, and install rules
    - Initialize Devbox with C++ development tools
    - Add a basic C++ example file
    - Include header file for parsing functionality
    - Add a shell script with commands for building, making, releasing, and compiling a mdbook

---
+++ run.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+
+# @cmd build cmake
+# @alias b
+# # @flag      --arrow        Enable gui support
+buildit() {
+    CURR_DIR=$(pwd)
+    if [[ ! -d "$TOP_HEAD/build" ]] then
+        mkdir "$TOP_HEAD/build";
+    else
+        rm -rf "$TOP_HEAD/build"
+        mkdir "$TOP_HEAD/build"
+    fi
+    cd "$TOP_HEAD/build"
+    cmake -Wno-dev ..
+    cd "$CURR_DIR"
+}
+
+
+# @cmd make project
+# @alias m
+makeit() {
+    CURR_DIR=$(pwd)
+    cd "$TOP_HEAD/build"
+    only_name=$(basename "$TOP_HEAD")
+    make 2>&1 | tee >(grep "^$TOP_HEAD" | grep -E "error:" > "$TOP_HEAD/.quickfix")
+    cd "$CURR_DIR"
+}
+
+
+# @cmd mark as releaser
+# @alias r
+# @arg type![patch|minor|major] Release type
+release() {
+    CURRENT_VERSION=$(grep -E '^project\(.*VERSION [0-9]+\.[0-9]+\.[0-9]+' CMakeLists.txt \
+        | sed -E 's/.*VERSION ([0-9]+\.[0-9]+\.[0-9]+).*/\1/')
+    IFS='.' read -r MAJOR MINOR PATCH <<< "$CURRENT_VERSION"
+    case $argc_type in
+        major)
+            MAJOR=$((MAJOR + 1))
+            MINOR=0
+            PATCH=0
+            ;;
+        minor)
+            MINOR=$((MINOR + 1))
+            PATCH=0
+            ;;
+        patch)
+            PATCH=$((PATCH + 1))
+            ;;
+    esac
+    version="$MAJOR.$MINOR.$PATCH"
+    sed -i -E "s/(project\(.*VERSION )[0-9]+\.[0-9]+\.[0-9]+/\1$version/" CMakeLists.txt
+    git cliff --tag $version > CHANGELOG.md
+    changelog=$(git cliff --unreleased --strip all)
+    git add -A && git commit -m "chore(release): prepare for $version"
+    echo "$changelog"
+    git tag -a $version -m "$version" -m "$changelog"
+    git push --follow-tags --force --set-upstream origin develop
+    gh release create $version --notes "$changelog"
+}
+
+
+# @cmd compile mdbook
+# @option    --dest_dir <dir>    Destination directory
+mdbook() {
+    mdbook build $TOP_HEAD/book --dest-dir $TOP_HEAD/docs
+    git add --all && git commit -m "docs: building website/mdbook"
+}
+
+
+eval "$(argc --argc-eval "$0" "$@")"
