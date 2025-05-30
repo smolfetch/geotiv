546290ede6c27a4d4cfec90e25ce63e8ee6d1ba4
Trim Bresilla <trim.bresilla@gmail.com>
2025-05-31 00:56:06 +0200

    build: add new function run and update build aliases
    - Update aliases for building, compiling, and running
    - Rename build and make functions
    - Add a run function
    - Improve changelog generation using git cliff to prepend content and handle first releases

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 
-# @cmd build cmake
-# @alias b
-# # @flag      --arrow        Enable gui support
-buildit() {
+# @cmd compile project
+# @alias c
+compileit() {
     CURR_DIR=$(pwd)
     if [[ ! -d "$TOP_HEAD/build" ]] then
         mkdir "$TOP_HEAD/build";
@@ -18,19 +17,26 @@ buildit() {
 }
 
 
-# @cmd make project
-# @alias m
-makeit() {
+# @cmd build project
+# @alias b
+buildit() {
     CURR_DIR=$(pwd)
     cd "$TOP_HEAD/build"
-    only_name=$(basename "$TOP_HEAD")
-    make 2>&1 | tee >(grep "^$TOP_HEAD" | grep -E "error:" > "$TOP_HEAD/.quickfix")
+    make
+    cd "$CURR_DIR"
+}
+
+
+# @cmd run project
+# @alias r
+runit() {
+    CURR_DIR=$(pwd)
+    $TOP_HEAD/build/./main
     cd "$CURR_DIR"
 }
 
 
 # @cmd mark as releaser
-# @alias r
 # @arg type![patch|minor|major] Release type
 release() {
     CURRENT_VERSION=$(grep -E '^project\(.*VERSION [0-9]+\.[0-9]+\.[0-9]+' CMakeLists.txt \
@@ -51,16 +57,25 @@ release() {
             ;;
     esac
     version="$MAJOR.$MINOR.$PATCH"
+        # Get the latest tag to create a range for changelog generation
+    LATEST_TAG=$(git tag --list --sort=-version:refname | head -n 1)
+    if [ -n "$LATEST_TAG" ]; then
+        # Get changelog content for release notes (changes since last tag)
+        changelog=$(git cliff $LATEST_TAG..HEAD --strip all)
+        # Generate changelog and prepend to existing file (changes since last tag)
+        git cliff --tag $version $LATEST_TAG..HEAD --prepend CHANGELOG.md
+    else
+        # First release - get all changes
+        changelog=$(git cliff --unreleased --strip all)
+        git cliff --tag $version --unreleased --prepend CHANGELOG.md
+    fi
     sed -i -E "s/(project\(.*VERSION )[0-9]+\.[0-9]+\.[0-9]+/\1$version/" CMakeLists.txt
-    git cliff --tag $version > CHANGELOG.md
-    changelog=$(git cliff --unreleased --strip all)
     git add -A && git commit -m "chore(release): prepare for $version"
     echo "$changelog"
     git tag -a $version -m "$version" -m "$changelog"
     git push --follow-tags --force --set-upstream origin develop
     gh release create $version --notes "$changelog"
 }
-
 
 # @cmd compile mdbook
 # @option    --dest_dir <dir>    Destination directory
